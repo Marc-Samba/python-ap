dafd74d4ccf67db7564e56c6a7a7deb7a2fa47c1
Marc Samba <marc.samba@etu.minesparis.psl.eu>
2023-11-07 17:41:10 +0100

    mise à jour 1

--- snakegame.py
+++ snakegame.py
@@ -8,11 +8,6 @@ fps=20
 screen = pygame.display.set_mode((screenwidth,screenheight))
 clock = pygame.time.Clock()
 
-for i in range (0,screenwidth,40):
-    for j in range (0, screenheight,40):
-        rect = pygame.Rect(i, screenheight-j , 20, 20)
-        pygame.draw.rect(screen, (0,0,0), rect)
-
 while True:
 
     clock.tick(fps)
