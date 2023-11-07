ae982cb46366f58b3e208c43cc217845e4d5057b
Marc Samba <marc.samba@etu.minesparis.psl.eu>
2023-11-07 17:40:01 +0100

    mise à jour

--- snakegame.py
+++ snakegame.py
@@ -1,18 +1,38 @@
 import pygame
 
 pygame.init()
-
-screen = pygame.display.set_mode( (640, 480) )
-
+screenwidth=400
+screenheight=300
+color=(255,255,255)
+fps=20
+screen = pygame.display.set_mode((screenwidth,screenheight))
 clock = pygame.time.Clock()
+
+for i in range (0,screenwidth,40):
+    for j in range (0, screenheight,40):
+        rect = pygame.Rect(i, screenheight-j , 20, 20)
+        pygame.draw.rect(screen, (0,0,0), rect)
 
 while True:
 
-    clock.tick(1)
+    clock.tick(fps)
 
     for event in pygame.event.get():
-        pass
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_q:
+                pygame.QUIT()
 
-    screen.fill( (255, 255, 255) )
+    screen.fill(color)
+    ligne=15
+    colonne=20
+    taillecase=screenwidth//colonne
+    for i in range (ligne):
+        for j in range (colonne):
+            if (i+j)%2==0:
+                rect= pygame.Rect(taillecase*j, taillecase*i , taillecase, taillecase)
+                pygame.draw.rect(screen, (0,0,0), rect)
+            else:
+                rect= pygame.Rect(taillecase*j, taillecase*i , taillecase, taillecase)
+                pygame.draw.rect(screen, (255,255,255), rect)
 
     pygame.display.update()
